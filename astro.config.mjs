c07e3431399a1627f258cfdc7484fd59281e0e99
Dmitrii Pavlov <dm.rom.pav@gmail.com>
2024-06-22 01:02:17 +0400

    wf

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dmrompav.github.io',
+  base: '/pavlov',
+
   integrations: [
     UnoCSS({
       injectReset: '@unocss/reset/tailwind.css'
